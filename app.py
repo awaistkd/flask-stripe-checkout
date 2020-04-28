aa822e7b785e054c403e3d398c373a94ba92a909
Muhammad Awais <aawais.nu@gmail.com>
2020-04-28 17:18:23 +0500

    added simple charge api

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import stripe
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 
 stripe_keys = {
     'secret_key': os.environ['STRIPE_SECRET_KEY'],
@@ -14,6 +14,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', key=stripe_keys['publishable_key'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+
+    amount = 500    # amount in cents
+    customer = stripe.Customer.create(
+        email='sample@customer.com',
+        source=request.form['stripeToken']
+    )
+
+    stripe.Charge.create(
+        customer=customer.id,
+        amount=amount,
+        currency='usd',
+        description='A Test Charge'
+    )
+
+    return render_template('charge.html', amount=amount)
+
 
 @app.route('/hello')
 def hello_world():
